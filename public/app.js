6e28dfdb9e4e5df0306b4ffdf4bc2179e5f41903
KugiHaito <kugihaito99@gmail.com>
2022-03-08 01:50:52 -0300

    refactor: app.js improvements

--- public/app.js
+++ public/app.js
@@ -1,45 +1,65 @@
-const getInputValue = document.querySelector("#query");
-const searchButton = document.querySelector("#btn-query");
-const getContainer = document.querySelector("#container");
+const app = {
 
-searchButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  getContainer.innerHTML = "";
-  const song = getInputValue.value;
-  fetch(`./api/search?q=${song}`)
-    .then((response) => response.json())
-    .then((data) =>
-      data.forEach((song) => {
-        createCard(song.id ,song.title, song.songArt, song.artistName);
+  /**
+   * Interface components
+   */
+  interface: {
+    container: "#container",
+    search: "#query"
+  },
+
+  actions: {
+
+    /**
+     * Search Event
+     * @param {PointerEvent} e event
+     */
+    search: e => {
+      e.preventDefault();
+      app.interface.container.innerHTML = "";
+      const song = app.interface.search.value;
+      fetch(`./api/search?q=${song}`)
+        .then((response) => response.json())
+        .then((data) => {
+          app.interface.container.innerHTML = data
+            .map(song => app.createCard(song)).join('\n')
+        });
+    },
+
+  },
+
+  /**
+   * create card element
+   * @param {object} song song data
+   * @returns string
+   */
+  createCard({id, title, songArt, artistName}) {
+    return `<a href="./song?id=${id}">
+      <div class="card">
+        <img src="${songArt}" alt="${title}">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${artistName}</p>
+        </div>
+      </div>
+    </a>`;
+  },
+
+  /**
+   * Start Application
+   */
+  start() {
+    Object.entries(app.interface)
+      .forEach(([elem, query]) => {
+        app.interface[elem] = document.querySelector(query)
       })
-    );
-});
-
-const createImgElement = (path) => {
-  const img = document.createElement("img");
-  img.src = path;
-  img.className = "card-img-top";
-  return img;
+    
+    Array.from(document.querySelectorAll(".btn[data-action]"))
+      .forEach(b => {
+        b.addEventListener("click", app.actions[b.dataset.action])
+      })
+  }
 };
 
-const createCard = (id, title, songArt, artistName) => {
-  const link = document.createElement("a");
-  const firstDiv = document.createElement("div");
-  firstDiv.className = "card";
-  link.href = `./song?id=${id}`;
-  const songImg = createImgElement(songArt);
-  const secondDiv = document.createElement("div");
-  secondDiv.className = "card-body";
-  const songTitle = document.createElement("h5");
-  songTitle.className = "card-title";
-  songTitle.innerText = title;
-  const nameArtist = document.createElement("p");
-  nameArtist.innerText = artistName;
-  nameArtist.className = "card-text";
-  link.appendChild(songTitle);
-  secondDiv.appendChild(link);
-  secondDiv.appendChild(nameArtist);
-  firstDiv.appendChild(songImg);
-  firstDiv.appendChild(secondDiv);
-  getContainer.appendChild(firstDiv);
-};
+
+app.start()
