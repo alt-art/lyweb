5ea4abe0866d8212cc3fa7d8843d29d9a72c34ea
Pedro Mendes <pedromendescraft@gmail.com>
2022-05-26 00:51:25 -0300

    style: Change indentation to 2 spaces

--- main.ts
+++ main.ts
@@ -13,84 +13,84 @@ const port: string = process.env.PORT || '3000';
 const app: Application = express();
 
 const redisClient = createClient({
-    url: process.env.REDIS_TLS_URL,
-    socket: { tls: true, rejectUnauthorized: false },
+  url: process.env.REDIS_TLS_URL,
+  socket: { tls: true, rejectUnauthorized: false },
 });
 
 redisClient.on('error', (err) => {
-    console.log('Redis error', err);
+  console.log('Redis error', err);
 });
 
 app.use(
-    helmet({
-        contentSecurityPolicy: {
-            directives: {
-                'img-src': ["'self'", '*.genius.com', '*.rapgenius.com'],
-                'script-src': ["'self'", "'unsafe-inline'"],
-            },
-        },
-        crossOriginResourcePolicy: {
-            policy: 'same-origin',
-        },
-        crossOriginEmbedderPolicy: false,
-    }),
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'img-src': ["'self'", '*.genius.com', '*.rapgenius.com'],
+        'script-src': ["'self'", "'unsafe-inline'"],
+      },
+    },
+    crossOriginResourcePolicy: {
+      policy: 'same-origin',
+    },
+    crossOriginEmbedderPolicy: false,
+  }),
 );
 app.use(express.static(path.resolve('./public')));
 app.use(express.json());
 
 const geniusAPI = axios.create({
-    baseURL: 'https://api.genius.com',
-    headers: {
-        Authorization: `Bearer ${authToken}`,
-    },
+  baseURL: 'https://api.genius.com',
+  headers: {
+    Authorization: `Bearer ${authToken}`,
+  },
 });
 
 app.get('/api/search', async (req, res) => {
-    const { q: query, page } = req.query;
-    try {
-        const { data } = await geniusAPI.get(`/search?q=${query}&page=${page}`);
-        const songs = data.response.hits.map((hit) => {
-            const {
-                song_art_image_thumbnail_url: songArt,
-                title_with_featured: title,
-                primary_artist: { name: artistName },
-                id,
-            } = hit.result;
-            return {
-                songArt,
-                title,
-                artistName,
-                id,
-            };
-        });
-        res.json(songs);
-    } catch (error) {
-        res.status(500).json({ ...error.response.data });
-    }
+  const { q: query, page } = req.query;
+  try {
+    const { data } = await geniusAPI.get(`/search?q=${query}&page=${page}`);
+    const songs = data.response.hits.map((hit) => {
+      const {
+        song_art_image_thumbnail_url: songArt,
+        title_with_featured: title,
+        primary_artist: { name: artistName },
+        id,
+      } = hit.result;
+      return {
+        songArt,
+        title,
+        artistName,
+        id,
+      };
+    });
+    res.json(songs);
+  } catch (error) {
+    res.status(500).json({ ...error.response.data });
+  }
 });
 
 app.get('/api/lyrics/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const cachedLyrics = await redisClient.get(`lyrics:${id}`);
-        if (cachedLyrics) {
-            res.json({ plain: cachedLyrics });
-            return;
-        }
-        const { data } = await geniusAPI.get(`/songs/${id}?text_format=plain`);
-        const {
-            song: { lyrics },
-        } = data.response;
-        await redisClient.setEx(`lyrics:${id}`, 60 * 60 * 48, lyrics.plain);
-        res.json(lyrics);
-    } catch (error) {
-        res.status(500).json({ ...error.response.data });
+  const { id } = req.params;
+  try {
+    const cachedLyrics = await redisClient.get(`lyrics:${id}`);
+    if (cachedLyrics) {
+      res.json({ plain: cachedLyrics });
+      return;
     }
+    const { data } = await geniusAPI.get(`/songs/${id}?text_format=plain`);
+    const {
+      song: { lyrics },
+    } = data.response;
+    await redisClient.setEx(`lyrics:${id}`, 60 * 60 * 48, lyrics.plain);
+    res.json(lyrics);
+  } catch (error) {
+    res.status(500).json({ ...error.response.data });
+  }
 });
 
 app.listen(port, async () => {
-    console.time('Redis connected');
-    await redisClient.connect();
-    console.timeEnd('Redis connected');
-    console.log(`Server listening on port ${port}`);
+  console.time('Redis connected');
+  await redisClient.connect();
+  console.timeEnd('Redis connected');
+  console.log(`Server listening on port ${port}`);
 });
